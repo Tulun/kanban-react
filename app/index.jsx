5d539793f57aa034a9c947b37a9083ccd88a9084
Tulun <jaykiraly@gmail.com>
2016-07-16 11:37:17 -0700

    Setup basic provider.

--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-if (process.env.NODE_ENV !== 'production') {
-  React.Perf = require('react-addons-perf');
-}
 
 ReactDOM.render(
-  <App />,
+  <Provider><App /></Provider>,
   document.getElementById('app')
 );
